21447b1a591cfa7506f793c3b1e4b156e4e1458c
Samuel Eze <125476417+dlawise@users.noreply.github.com>
2023-10-04 01:19:28 +0100

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,6 +0,0 @@
-#Install Flask using pip3 with the specified version (2.1.0)
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,7 +0,0 @@
-#Create a manifest that kills process named killmenow
-exec { 'kill_killmenow_process':
-  command     => 'pkill -f "killmenow"',
-  path        => ['/usr/bin', '/bin'],
-  onlyif      => 'pgrep -f "killmenow"',
-  refreshonly => true,
-}
